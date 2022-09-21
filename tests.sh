cb5ded420284bbf55e22cad2dcce38717905cffe
Kaung L <kaung.lin@gmail.com>
2022-09-21 14:10:30 +0700

    All inclusive tests

--- tests.sh
+++ tests.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-
-echo "--- Test for single URI; sets default dir and filename ---"
-./download.sh -u "sample.txt"
+echo "--- Test for single URI; sets default dir and filename; download fails, removes file ---"
+./download.sh -u "example.com/sample.txt"
 echo "---"
 echo ""
 
-echo "--- Test for single URI with custom filename and protocol ---"
-./download.sh -o "output.txt" -u "sample.txt" -p "ftp"
+echo "--- Test for single URI with protocol; removes existing file and downloads; fails, removes file ---"
+./download.sh -u "www.nasdaq.com/market-activity/stocks/aapl" -p "http"
 echo "---"
 echo ""
 
-echo "--- Test for multiple URIs via config file ---"
-./download.sh -c uris.cfg
+echo "--- Test for single URI with sftp protocol ---"
+./download.sh -u "sftpuser@kaung.dev/text/hello.txt" -p "sftp" --hostpubsha256 $KEY
 echo "---"
 echo ""
+
+echo "--- Test for parallel download; multiple URIs/protocols via config file ---"
+./download.sh -c test_uris.cfg
+echo "---"
+echo ""
+
